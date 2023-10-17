a86397071f44a0b7429bf5c9dd3d451ab6834821
Thaslima <nizuthasli15@gmail.com>
2023-10-17 08:11:38 +0530

    ot17

--- pyra_finance_backend/utils/generateShortRandomName.js
+++
@@ -1,7 +0,0 @@
-const { v4: uuidv4 } = require('uuid');
-function generateShortRandomName() {
-  const uuid = uuidv4();
-  const shortName = uuid.replace(/-/g, '').substr(0, 10);
-  return shortName;
-}
-module.exports = generateShortRandomName;
